b02bef6806fc3cb081fba73109cf76c6b9b0a040
Zaziba Ahmed <ahmedzaziba@gmail.com>
2026-01-28 17:22:27 -0500

    Updated pipeline from "test"

--- config.js
+++ config.js
@@ -2,5 +2,5 @@
 const config = {
     accessToken: "xx51fe1f8b-5204-44fd-bce8-47bca909c080",
     organizationId: "pq6dopunremjf62dr2y33kdcgo4",
-    pipeline: "test"
+    pipeline: "decathlon-products-en" // test , decathlon-products-en , decathlon-products-fr
 };
